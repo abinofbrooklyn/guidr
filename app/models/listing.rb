03069836cfe83d14b8292cbf9e8bda36b7770454
abinofbrooklyn <abin.abraham4@gmail.com>
2014-08-19 12:49:58 -0400

    Create Search Functionality Create search results control Update listings model with search methods Create search form partial Create search results partial Add search and home links to application Create search results show page

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,8 +1,29 @@
 class Listing < ActiveRecord::Base
   belongs_to :user
+  has_many :reservations
 
   validates :city, presence: true
   validates :address, presence: true
   validates :title, presence: true
   validates :user, presence: true
+
+  SEARCH_RANGE = 0..99
+
+  def self.search(search_params)
+    city(search_params[:city])
+  end
+
+  def self.city(city)
+    query_if_present(city) do |f|
+      where("city ILIKE ?", "%#{f}%")
+    end
+  end
+
+  def self.query_if_present(term, &block)
+    if term.present?
+      block.call(term)
+    else
+      all
+    end
+  end
 end
