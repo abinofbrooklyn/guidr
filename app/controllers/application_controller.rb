dc2f3f1867ffbd672664407d8e8e00b16acbe8cb
abinofbrooklyn <abin.abraham4@gmail.com>
2014-08-18 20:03:06 -0400

    Create Reservations Table Create Search results migration Adjust Search results model config routes for reservations Implement before_login for application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,10 @@
 class ApplicationController < ActionController::Base
   include Monban::ControllerHelpers
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
+  before_action :require_login
+  
   protect_from_forgery with: :exception
 
   def current_user
       super || Guest.new
   end
-
 end
